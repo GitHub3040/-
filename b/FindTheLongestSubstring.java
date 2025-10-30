ea6e9a0522ca8d4a81569a5f78ffe6e1cc0b49ee
huangjiajing <3223573441@qq.com>
2025-10-30 10:00:23 +0800

    2025/10/30 提交一

--- b/FindTheLongestSubstring.java
+++ b/FindTheLongestSubstring.java
@@ -1,9 +1,35 @@
-//import java.util.HashMap;
-//
-//public class FindTheLongestSubstring {
-//    public int findTheLongestSubstring(String s) {
-//        HashMap<Integer,Integer> map=new HashMap<>();
-//
-//    }
-//
-//}
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class FindTheLongestSubstring {
+    public int findTheLongestSubstring(String s) {
+        int n=s.length();
+        int [] map=new int[32];
+        int ans=0;
+        Arrays.fill(map,-2);
+        map[0]=-1;
+        for (int i = 0,status=0; i < n; i++) {
+            int m=charaAt(s.charAt(i));
+            if (m!=-1){
+                status^=(1<<m);
+            }
+            if (map[status]!=-2){
+                ans=Math.max(ans,i-map[status]);
+            }else {
+                map[status]=i;
+            }
+        }
+        return ans;
+    }
+    public int charaAt(char ch){
+        switch (ch){
+            case 'a': return 0;
+            case 'e': return 1;
+            case 'i': return 2;
+            case 'o': return 3;
+            case 'u': return 4;
+            default:return -1;
+        }
+    }
+
+}
