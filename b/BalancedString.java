4df02a6580f4661cf0eb1b63e59d9ba5bd56a97d
huangjiajing <3223573441@qq.com>
2025-11-03 20:28:58 +0800

    2025/11/3

--- b/BalancedString.java
+++ b/BalancedString.java
@@ -1,6 +1,41 @@
 import java.util.HashMap;
 
 public class BalancedString {
+    public static int balancedString1(String s){
+        int n=s.length();
+        int [] map=new int[n];
+        int[] cnt=new int[4];
+        for (int i = 0; i < n; i++) {
+            char c=s.charAt(i);
+            map[i]=c=='W' ? 1 : c=='E' ? 2 : c=='R' ? 3 : 0;
+            cnt[map[i]]++;
+        }
+        int debt=0;
+        for (int i = 0; i < 4; i++) {
+            if (cnt[i]< n/4){
+                cnt[i]=0;
+            }else {
+                cnt[i] = n / 4 - cnt[i];
+                debt -= cnt[i];
+            }
+        }
+        if (debt == 0) {
+            return 0;
+        }
+        int ans = Integer.MAX_VALUE;
+        for (int l = 0,r=0; r < n; r++) {
+            if (cnt[map[r]]++<0){
+                debt--;
+            }
+            if (debt==0){
+                while (cnt[map[l]]>0){
+                    cnt[map[l++]]--;
+                }
+                ans = Math.min(ans, r - l + 1);
+            }
+        }
+        return ans;
+    }
     public static int balancedString(String s) {
         int n=s.length();
         int m=n/4,left=0,ans=n,size=0;
