b352ffaf2069093b976e9c7eba60cd00ab06f2af
huangjiajing <3223573441@qq.com>
2025-11-03 18:17:42 +0800

    2025/11/3

--- b/CanCompleteCircuit.java
+++ b/CanCompleteCircuit.java
@@ -10,4 +10,18 @@ public class CanCompleteCircuit {
         }
         return start>=n ? -1 : n;
     }
+    public int canCompleteCircuit1(int[] gas, int[] cost){
+        int n=gas.length;
+        for (int l=0,r=0,sum; l<n ; l=r+1,r=l) {
+            sum=0;
+            while (sum+gas[r%n]-cost[r%n]>=0){
+                if (r-l+1==n){
+                    return l;
+                }
+                sum+=gas[r%n]-cost[r%n];
+                r++;
+            }
+        }
+        return -1;
+    }
 }
